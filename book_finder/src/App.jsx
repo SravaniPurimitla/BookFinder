6486c0eb3e05dee9838256760226c31572a166c0
SravaniPurimitla <sravani235p@gmail.com>
2025-10-28 11:21:48 +0530

    updates

--- book_finder/src/App.jsx
+++ book_finder/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-800">
       <header className="bg-indigo-600 text-white py-4 shadow-md">
-        <h1 className="text-center text-3xl font-semibold">📚 Book Finder</h1>
+        <h1 className="text-center text-3xl font-semibold">Book Finder</h1>
       </header>
       <Home />
     </div>
